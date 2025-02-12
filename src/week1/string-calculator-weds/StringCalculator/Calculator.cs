48c645833c1fe7df5304d6a53949b2aab9efc93d
AvaSchneider <packyschneider@gmail.com>
2025-02-12 16:58:43 -0500

    end of day 3

---
+++ src/week1/string-calculator-weds/StringCalculator/Calculator.cs
@@ -0,0 +1,40 @@
+﻿
+public class Calculator
+{
+    public int Add(string numbers)
+    {
+        if (numbers == "")
+            return 0;
+        else
+        {
+            if (!numbers.Contains(",") && !numbers.Contains('\n'))
+            {
+                return int.Parse(numbers);
+            }
+            else 
+            {
+                char[] delimiters = new char[0];
+
+                if (!numbers.Contains('\n'))
+                {
+                    delimiters = new char[] { ',' };
+                }
+                else if (!numbers.Contains(","))
+                {
+                    delimiters = new char[] { '\n' };
+                }
+                else { 
+                    delimiters = new char[] {',', '\n'};
+                }
+                string[] individualNumbers = numbers.Split(delimiters);
+                int addedNumbers = 0;
+                foreach (string num in individualNumbers)
+                { 
+                    int number = int.Parse(num);
+                    addedNumbers += number;
+                }
+                return addedNumbers;
+            }
+        }
+    }
+}
---
+++ src/week1/string-calculator-weds/StringCalculator/CalculatorTests.cs
@@ -0,0 +1,70 @@
+﻿
+
+namespace StringCalculator;
+public class CalculatorTests
+{
+    [Fact]
+    public void EmptyStringReturnsZero()
+    {
+        var calculator = new Calculator();
+
+        var result = calculator.Add("");
+
+        Assert.Equal(0, result);
+    }
+
+    [Theory]
+    [InlineData("1", 1)]
+    [InlineData("2", 2)]
+    [InlineData("3256", 3256)]
+    [InlineData("-42", -42)]
+    public void SingleDigit(string numbers, int expected)
+    {
+        var calculator = new Calculator();
+
+        var result = calculator.Add(numbers);
+
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("1,2", 3)]
+    [InlineData("3,4", 7)]
+    [InlineData("328,239", 567)]
+    [InlineData("-42,15", -27)]
+    [InlineData("-12,-4", -16)]
+    public void TwoDigits(string numbers, int expected)
+    {
+        var calculator = new Calculator();
+
+        var result = calculator.Add(numbers);
+
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("1,2,3", 6)]
+    [InlineData("3,4,5,6,7,8", 33)]
+    [InlineData("328,239,4,-3,20,-17", 571)]
+    [InlineData("-42,15,-3", -30)]
+    [InlineData("-12,-4,-8", -24)]
+    public void MoreThanTwoDigits(string numbers, int expected)
+    {
+        var calculator = new Calculator();
+
+        var result = calculator.Add(numbers);
+
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("1,2\n3", 6)]
+    public void TwoDelimiters(string numbers, int expected)
+    {
+        var calculator = new Calculator();
+
+        var result = calculator.Add(numbers);
+
+        Assert.Equal(expected, result);
+    }
+}
